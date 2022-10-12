403c955fffc526444f2e37cf1dfabdca432f040e
CesarWatrin <cesar.watrin@hotmail.com>
2022-10-12 17:13:02 +0200

    built: config eslint/prettier

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
+import { useHead } from '@vueuse/head';
 useHead({
   title: 'App title',
-  meta: [
-    { name: 'description', content: 'App title' },
-  ],
-})
+  meta: [{ name: 'description', content: 'App title' }],
+});
 </script>
 
 <template>
