1f8bd5e98c516d48ffe2d2d7a742b7c42fa92e50
CesarWatrin <cesar.watrin@hotmail.com>
2022-10-23 16:32:57 +0200

    feat(layout): footer done

--- src/common/components/organisms/Footer.vue
+++ src/common/components/organisms/Footer.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>footer</div>
+  <div
+    class="bg-main-secondary absolute z-0 bottom-0 w-full flex justify-around md:justify-between pb-14 md:pb-0"
+  >
+    <div class="w-24 md:w-60">
+      <RouterLink to="#">
+        <img class="w-20 mx-auto my-14" src="src/common/assets/images/CSIM.svg" alt="csim-logo" />
+      </RouterLink>
+    </div>
+    <div class="flex justify-around justify-center text-white text-md mt-8 w-2/3 md:w-1/2">
+      <div class="flex flex-col">
+        <h3 class="font-bold">Company</h3>
+        <RouterLink class="mt-2" to="#">About us</RouterLink>
+        <RouterLink class="mt-2" to="#">Contact us</RouterLink>
+      </div>
+      <div class="flex flex-col">
+        <h3 class="font-bold">Company</h3>
+        <RouterLink class="mt-2" to="#">CGU</RouterLink>
+        <RouterLink class="mt-2" to="#">Cookies</RouterLink>
+        <RouterLink class="mt-2" to="#">Terms and Privacy</RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
--- src/common/components/organisms/Header.vue
+++ src/common/components/organisms/Header.vue
@@ -3,9 +3,9 @@ import { faNewspaper, faWallet, faChartLine } from '@fortawesome/free-solid-svg-
 </script>
 <template>
   <div class="relative top-0 bg-white h-14 w-full flex justify-between bg-main-secondary">
-    <div class="flex justify-center p-2 md:ml-8">
-      <img src="src/common/assets/images/CSIM.svg" alt="" />
-    </div>
+    <RouterLink to="#" class="flex justify-center p-2 md:ml-8">
+      <img src="src/common/assets/images/CSIM.svg" alt="csim-logo" />
+    </RouterLink>
     <div class="flex justify-center items-center text-white p-2 md:mr-8">
       <div class="hidden md:flex mr-8">
         <RouterLink class="py-4 px-6" to="#">market</RouterLink>
@@ -13,16 +13,16 @@ import { faNewspaper, faWallet, faChartLine } from '@fortawesome/free-solid-svg-
         <RouterLink class="py-4 px-6" to="">wallet</RouterLink>
       </div>
       <div class="flex flex-col text-xs">
-        <small class="hidden md:block">Bienvenue</small>
+        <small class="hidden md:block">Welcome</small>
         <span class="text-xs font-bold">Michael Smith</span>
       </div>
       <div class="ml-2">
-        <img src="src/common/assets/images/user.png" alt="" />
+        <img src="src/common/assets/images/user.png" alt="user-photo" />
       </div>
     </div>
   </div>
   <div
-    class="flex justify-around bg-main-secondary text-white text-center absolute w-full bottom-0 text-xl cursor-pointer md:hidden"
+    class="flex justify-around bg-main-secondary text-white text-center absolute z-50 w-full bottom-0 text-xl cursor-pointer md:hidden"
   >
     <RouterLink to="#" class="w-1/3 py-3 hover:text-main-primary group">
       <div class="max-w-fit mx-auto">
