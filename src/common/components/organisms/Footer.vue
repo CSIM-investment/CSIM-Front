45a5d84af66076dd675c7c8213126f156f1aaab0
CesarWatrin <cesar.watrin@hotmail.com>
2022-10-23 16:48:12 +0200

    feat(layout): layout improvements

--- src/common/components/organisms/Footer.vue
+++ src/common/components/organisms/Footer.vue
@@ -4,10 +4,16 @@
   >
     <div class="w-24 md:w-60">
       <RouterLink to="#">
-        <img class="w-20 mx-auto my-14" src="src/common/assets/images/CSIM.svg" alt="csim-logo" />
+        <img
+          class="w-14 md:w-20 mx-auto my-14"
+          src="src/common/assets/images/CSIM.svg"
+          alt="csim-logo"
+        />
       </RouterLink>
     </div>
-    <div class="flex justify-around justify-center text-white text-md mt-8 w-2/3 md:w-1/2">
+    <div
+      class="flex justify-around justify-center text-white text-xs md:text-sm mt-8 w-2/3 md:w-1/2"
+    >
       <div class="flex flex-col">
         <h3 class="font-bold">Company</h3>
         <RouterLink class="mt-2" to="#">About us</RouterLink>
