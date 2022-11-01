c80bb1c92c7a87acf093569016305cd114d8f35d
luca-pi <luca@pilloni.fr>
2022-11-01 21:14:46 +0100

    feature(auth): add login and signin page

---
+++ src/authentication/organisms/AuthLayout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import authIllustration from '../../common/assets/illustrations/auth_illustration.svg';
+import logoPrimary from '../../common/assets/logos/logo_primary.svg';
+
+const { t } = useI18n();
+
+const props = defineProps<{
+  title: string;
+  subtitle: string;
+}>();
+</script>
+<template>
+  <div class="flex h-screen">
+    <section class="bg-primary flex flex-col flex-1 items-center justify-center gap-2 p-8">
+      <img :src="authIllustration" alt="" class="w-full max-w-2xl" />
+      <p class="text-white text-2xl text-center">{{ t('auth.description') }}</p>
+    </section>
+    <section class="flex-1 px-28 flex items-center justify-center">
+      <div class="flex flex-col items-center justify-center w-2/3 text-center">
+        <img :src="logoPrimary" alt="" />
+        <h1 class="text-5xl font-bold pt-4 pb-2">{{ props.title }}</h1>
+        <h2 class="text-3xl text-grey-dark font-semibold pb-12">
+          {{ props.subtitle }}
+        </h2>
+        <slot />
+      </div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ src/authentication/pages/login.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import AuthLayout from '../organisms/AuthLayout.vue';
+const { t } = useI18n();
+const loginForm = ref({
+  email: '',
+  password: '',
+});
+
+function onLogin(event: FormDataEvent) {
+  console.log(loginForm.value);
+}
+</script>
+<template>
+  <AuthLayout :title="t('login.title')" :subtitle="t('login.subtitle')">
+    <form class="flex flex-col items-center gap-8 w-full" @submit.prevent="onLogin">
+      <InputText type="text" class="text-input" placeholder="Email" v-model="loginForm.email" />
+      <Password
+        class="password-input"
+        placeholder="Password"
+        v-model="loginForm.password"
+        toggleMask
+        :feedback="false"
+      />
+      <Button class="button w-2/3 mb-4" type="submit">Connexion</Button>
+    </form>
+    <p>
+      Pas de compte ?
+      <RouterLink to="/signin" class="font-bold">S'inscrire</RouterLink>
+    </p>
+  </AuthLayout>
+</template>
