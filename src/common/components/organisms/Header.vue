fee827a70fc65dfdbbaaecc7a07e3381dda154df
CesarWatrin <cesar.watrin@hotmail.com>
2022-11-11 18:12:43 +0100

    feat(layout): routing link and css

--- src/common/components/organisms/Header.vue
+++ src/common/components/organisms/Header.vue
@@ -1,23 +1,29 @@
 <script lang="ts" setup>
-import { faNewspaper, faWallet, faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine, faNewspaper, faWallet } from '@fortawesome/free-solid-svg-icons'
 </script>
 <template>
   <div class="relative top-0 h-16 w-full flex justify-between bg-main-secondary">
-    <RouterLink to="#" class="flex justify-center p-2 md:ml-8">
-      <img src="src/common/assets/images/CSIM.svg" alt="csim-logo" />
+    <RouterLink to="/" class="flex justify-center p-2 md:ml-8">
+      <img src="src/common/assets/images/CSIM.svg" alt="csim-logo">
     </RouterLink>
     <div class="flex justify-center items-center text-white p-2 md:mr-8">
       <div class="hidden md:flex mr-8">
-        <RouterLink class="py-4 px-6" to="#">market</RouterLink>
-        <RouterLink class="py-4 px-6" to="">exchange</RouterLink>
-        <RouterLink class="py-4 px-6" to="">wallet</RouterLink>
+        <RouterLink class="py-4 px-6 hover:text-main-primary" to="market">
+          market
+        </RouterLink>
+        <RouterLink class="py-4 px-6 hover:text-main-primary" to="exchange">
+          exchange
+        </RouterLink>
+        <RouterLink class="py-4 px-6 hover:text-main-primary" to="wallet">
+          wallet
+        </RouterLink>
       </div>
       <div class="flex flex-col text-xs">
         <small class="hidden md:block">Welcome</small>
         <span class="text-xs font-bold">Michael Smith</span>
       </div>
       <div class="ml-2">
-        <img src="src/common/assets/images/user.png" alt="user-photo" />
+        <img src="src/common/assets/images/user.png" alt="user-photo">
       </div>
     </div>
   </div>
@@ -27,19 +33,19 @@ import { faNewspaper, faWallet, faChartLine } from '@fortawesome/free-solid-svg-
     <RouterLink to="#" class="w-1/3 py-3 hover:text-main-primary group">
       <div class="max-w-fit mx-auto">
         <font-awesome-icon :icon="faNewspaper" />
-        <hr class="invisible group-hover:visible" />
+        <hr class="invisible group-hover:visible">
       </div>
     </RouterLink>
     <RouterLink to="#" class="w-1/3 py-3 hover:text-main-primary group">
       <div class="max-w-fit mx-auto">
         <font-awesome-icon :icon="faWallet" />
-        <hr class="invisible group-hover:visible" />
+        <hr class="invisible group-hover:visible">
       </div>
     </RouterLink>
     <RouterLink to="#" class="w-1/3 py-3 hover:text-main-primary group">
       <div class="max-w-fit mx-auto">
         <font-awesome-icon :icon="faChartLine" />
-        <hr class="invisible group-hover:visible" />
+        <hr class="invisible group-hover:visible">
       </div>
     </RouterLink>
   </div>
