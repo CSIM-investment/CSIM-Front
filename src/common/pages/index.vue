2ea74860afd8e0df5c42a2b26f3a167f93cb2bab
CesarWatrin <cesar.watrin@hotmail.com>
2023-01-22 16:17:33 +0100

    feat: various home page ux/ui improvements

--- src/common/pages/index.vue
+++ src/common/pages/index.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts" xmlns="http://www.w3.org/1999/html">
 const { t } = useI18n()
-const router = useRouter();
+const router = useRouter()
 const columns = ref([
-  {field: 'nom', header: 'Nom'},
-  {field: 'price', header: 'Prix'},
-  {field: 'changement', header: 'Changement 24h'},
+  { field: 'nom', header: 'Nom' },
+  { field: 'price', header: 'Prix' },
+  { field: 'changement', header: 'Changement 24h' },
 ])
 
 const data = ref([
@@ -54,9 +54,11 @@ function AmountColor(data: any) {
 }
 </script>
 <template>
-  <article class="scroller">
+  <article>
     <div class="flex-col">
-      <section class="flex-col background-landing-page pt-10">
+      <section class="flex-col background-landing-page pt-20">
+        <img class="absolute top-52 md:left-10 xl:left-36 xl:animate-bounce" src="/src/common/assets/images/bitcoin.png" alt="bitcoin">
+        <img class="absolute top-64 md:right-10 xl:right-64 xl:animate-bounce" src="/src/common/assets/images/ethereum.png" alt="ethereum">
         <div class="flex-col text-center pt-10">
           <h1 class=" text-2xl sm:text-5xl font-black">
             {{ t('index.title') }}
@@ -64,11 +66,13 @@ function AmountColor(data: any) {
           <h2 class="text-xl sm:text-3xl font-black pt-10 sub-title text-color-grey">
             {{ t('index.subtitle') }}
           </h2>
-          <p class="pt-10 sm:text-xl">
+          <p class="py-10 sm:text-xl">
             {{ t('index.slogan') }}
           </p>
-          <Button @click="router.push('/login')" class="button mx-auto xl:w-1/6 xs:2/6 mt-15 font-bold mb-4 mt-10"
-                  type="submit">
+          <Button
+            class="button mx-auto xl:w-1/6 xs:2/6 font-bold mb-4 mt-1" type="submit"
+            @click="router.push('/login')"
+          >
             {{ t('index.button') }}
           </Button>
         </div>
@@ -77,7 +81,7 @@ function AmountColor(data: any) {
             <img src="/public/bitcoin.png" alt="bitcoin-logo" class="mr-3" width="40px" height="40px">
             <span> Bitcoin </span>
           </div>
-          <div class="flex items-center sm:pr-10">
+          <div class="flex items-center pr-10">
             <img src="/public/etherium.png" alt="etherium-logo" class="mr-3" width="40px" height="40px">
             <span> Etherium </span>
           </div>
@@ -87,17 +91,17 @@ function AmountColor(data: any) {
           </div>
         </div>
         <div class="bottom-0 w-full transition-all duration-300 ease-in-out delay-150 ">
-          <span/>
-          <div class="wave smt-10"/>
+          <span />
+          <div class="wave smt-10" />
         </div>
       </section>
-      <section>
-        <div class="flex-col list-crypto">
-          <DataTable class="rounded-lg" v-if="data" :value="data" responsive-layout="scroll">
+      <section class="background-connector-darker">
+        <div class="flex-col list-crypto w-full lg:max-w-3xl mx-auto">
+          <DataTable v-if="data" :value="data">
             <Column field="nom" header="Nom" style="min-width: 14rem">
               <template #body="{ data }">
                 <div class="flex">
-                  <Image class="mr-2" :src="data.nom.image" width="30" height="30" :alt="data.nom.nom"/>
+                  <Image class="mr-2" :src="data.nom.image" width="30" height="30" :alt="data.nom.nom" />
                   <span class="">{{ data.nom.nom }}</span>
                 </div>
               </template>
@@ -110,22 +114,22 @@ function AmountColor(data: any) {
             <Column field="changement" header="Changement 24h" style="min-width: 14rem">
               <template #body="{ data }">
                 <div class="flex align-middle items-center">
-                  <font-awesome-icon class="mr-2" :class="AmountColor(data.changement)" icon="fa-solid fa-caret-up"/>
+                  <font-awesome-icon class="mr-2" :class="AmountColor(data.changement)" icon="fa-solid fa-caret-up" />
                   <span :class="AmountColor(data.changement)"> {{ data.changement.symbole }} {{
-                      data.changement.amount
-                    }}</span>
+                    data.changement.amount
+                  }}</span>
                 </div>
               </template>
             </Column>
           </DataTable>
         </div>
       </section>
-      <section>
+      <section class="background-connector-bottom">
         <div class="flex justify-center xs:text-center">
-          <span class="xl:text-3xl xs:text-2xl font-bold mb-7">{{ t('index.catchPhrase') }}</span>
+          <span class="xl:text-3xl xs:text-2xl font-bold my-32">{{ t('index.catchPhrase') }}</span>
         </div>
-        <div class="xl:flex-row md:flex-row sm:flex-row flex xs:flex-col card-container justify-around mb-10">
-          <div class="card-pres flex-col flex xl:w-1/4 xs:w-full xs:mt-7 mt-8 justify-evenly rounded-lg">
+        <div class="flex flex-wrap card-container justify-around">
+          <div class="card-pres max-w-md mt-8 justify-evenly rounded-lg p-4">
             <img class="gif mt-7 mb-7 w-1/2" alt="bitcoin-logo" src="/src/common/assets/images/bitcoin.svg">
             <div class="mt-7 mr-5 ml-5">
               <span class="font-bold">{{ t('index.container1.title') }} </span>
@@ -134,7 +138,7 @@ function AmountColor(data: any) {
               </p>
             </div>
           </div>
-          <div class="card-pres xl:w-1/4 xs:mt-7 flex flex-col justify-evenly rounded-lg">
+          <div class="card-pres max-w-md mt-8 justify-evenly rounded-lg p-">
             <div class="mt-7 mb-3 mr-5 ml-5">
               <span class="font-bold">{{ t('index.container2.title') }}</span>
               <p class="mt-7 paragraphe">
@@ -143,7 +147,7 @@ function AmountColor(data: any) {
             </div>
             <img class="gif w-1/2" alt="window-svg" src="/src/common/assets/images/window.svg" width="50%" height="50%">
           </div>
-          <div class="card-pres xl:w-1/4 xs:w-full flex flex-col mt-8 justify-evenly rounded-lg">
+          <div class="card-pres max-w-md mt-8 justify-evenly rounded-lg p-">
             <img class="gif mt-7 mb-5 w-1/2" alt="dashboard-svg" src="/src/common/assets/images/dashboard.svg">
             <div class="mb-5 mr-5 ml-5">
               <span class="font-bold">{{ t('index.container3.title') }}</span>
@@ -155,19 +159,21 @@ function AmountColor(data: any) {
         </div>
       </section>
       <section>
-        <div class="w-full flex flex-col justify-center connect-container">
+        <div class="w-full flex flex-col justify-center background-connector">
           <div
-              class="md:text-2xl xl:text-3xl sm:text-xl xs:text-xl mt-10 text-center font-bold text-white xs:mr-5 xs:ml-5 self-center"
+            class="md:text-2xl xl:text-3xl sm:text-xl xs:text-xl mt-36 text-center font-bold xs:mr-5 xs:ml-5 self-center"
           >
             {{ t('index.subSlogan') }}
           </div>
-          <Button @click="router.push('/login')" class="mb-10 button self-center xl:w-1/6 sm:w-1/2 mt-15 font-bold mb-4 mt-10"
-                  type="submit">
+          <Button
+            class="mb-10 button self-center xl:w-1/6 sm:w-1/2 mt-15 font-bold mb-4 mt-10" type="submit"
+            @click="router.push('/login')"
+          >
             {{ t('index.button') }}
           </Button>
         </div>
       </section>
-      <Footer/>
+      <Footer />
     </div>
   </article>
 </template>
@@ -176,10 +182,16 @@ function AmountColor(data: any) {
   background: linear-gradient(307.57deg, rgba(142, 110, 202, 0.29) 21.99%, rgba(255, 255, 255, 0) 75.6%);
 }
 
-.scroller {
-  height: 100vh;
-  overflow-y: scroll;
-  scroll-snap-type: y mandatory;
+.background-connector {
+  background: linear-gradient(rgba(142, 110, 202, 0.29) 21.99%, rgba(255, 255, 255, 0) 75.6%);
+}
+
+.background-connector-bottom {
+  background: linear-gradient(to top, rgba(142, 110, 202, 0.29) 21.99%, rgba(255, 255, 255, 0) 75.6%);
+}
+
+.background-connector-darker {
+  background: linear-gradient(rgba(142, 110, 202) 21.99%, rgba(255, 255, 255, 0) 75.6%);
 }
 
 .scroller section {
@@ -217,9 +229,6 @@ function AmountColor(data: any) {
 
 .list-crypto {
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-  width: 50%;
-  margin: 20px auto;
 }
 
 .card-pres {
