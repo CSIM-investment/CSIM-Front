853ad89d419869d035830b19c6d9821e0e24c7f2
doninj <46936156+doninj@users.noreply.github.com>
2023-05-09 19:56:49 +0200

    Hotfix/table list invest (#31)
    * fix: table date
    
    * feat: modif table
    
    * fix: refresh user

--- src/investments/pages/Investments.vue
+++ src/investments/pages/Investments.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import { useAccountInvestmentsQuery } from '~/common/generated/graphql'
+import { useSessionStore } from '~/authentication/stores/session'
 
 const { t } = useI18n()
 
 const { result: investmentsList, refetch: refetchInvestements } = useAccountInvestmentsQuery()
+const { user, fetchSession } = useSessionStore()
 
 const templateLink = ref<HTMLAnchorElement>()
 
+async function refetch() {
+  await refetchInvestements()
+  await fetchSession()
+}
 function onClickTemplateDownload() {
   templateLink.value?.click()
 }
@@ -22,7 +28,7 @@ function onClickTemplateDownload() {
     </h2>
     <InvestmentFilesDrop
       @template-download="onClickTemplateDownload"
-      @files-imported="refetchInvestements"
+      @files-imported="refetch"
     />
     <a ref="templateLink" href="/investment-template.csv" download />
     <InvestmentList :list="investmentsList?.account?.investments ?? []" />
